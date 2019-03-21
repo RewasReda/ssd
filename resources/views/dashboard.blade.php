f36b7e729334dfd738e96bb72c1ce72c8deb487a
nouranaborwash@gmail.com <nouranaborwash@gmail.com>
2019-03-21 14:00:13 +0200

    woOOOohooo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashbosdasdard</div>
+                <div class="card-header">Dashbosduhuikhjhasdard</div>
 
                 <div class="card-body">
                     @if (session('status'))
