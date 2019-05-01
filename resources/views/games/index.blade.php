1bdea8953479d2c1cd8a7c08abead8756b34005c
AhmedELSamY10 <ahmedsamyaaa10@gmail.com>
2019-05-01 18:46:46 +0200

    UI DYNAMIC UPDTES

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -12,7 +12,7 @@
             
             @csrf
         @foreach ($games as $game)
-            <div class="card card-body bg-light">
+            {{-- <div class="card card-body bg-light">
                 <div class="card card-body bg-light">
                     <div class="col-md-4 col-sm-4">
                         <img style="width:100%" src="storage/cover_images/{{$game->cover_image}}">
@@ -25,7 +25,38 @@
                 <div>
                     <input name = {{ $game->id }} type="checkbox" class="big_check">
                 </div>
+            </div> --}}
+
+
+            <div class="border border-white">
+                    <div class="card-deck" style="width:500px">
+                            {{-- <div class="card" style="width:50%"> --}}
+
+                  <div class="card-body" >
+
+                    <div class = "float-left">
+                      <strong class="text-primary">{{$game->category}}</strong>
+                      <h3 ><a href="/games/{{$game->id}}">{{$game->title}}</a></h3>
+                      <div class="text-muted">{{$game->created_at}}</div>
+                    </div>
+
+                    <div class = "float-right">
+                    <img style="width:200px" src="storage/cover_images/{{$game->cover_image}}">
+                    </div>
+                    
+
+                    </div>
+                    <div>
+                            <input name = {{ $game->id }} type="checkbox" class="big_check">
+                        </div>
+                {{-- </div> --}}
             </div>
+
+
+        </div>
+
+
+
         @endforeach
             <button type ="submit" class="btn btn-primary">Finish Request</button>
         </form>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,135 +9,53 @@
         </div>
         <div class="product-device box-shadow d-none d-md-block"></div>
         <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
-        
+
       </div>
+        <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+            <div class="col-md-6 px-0">
+              <h1 class="display-4 font-italic">From Game Player to Game Developer</h1>
+              <p class="lead my-3">the only thing more fun than playing games is making them. You can make games. All it takes is some time, a willingness to learn and a passion to create. You don't need to be a "coder" to make games. Part of the beauty of games is that they take a variety of skills to make. Art, creativity, and systems thinking are just as important as code. Join us in this journey into game making! </p>
+        
+            </div> 
+            
+          </div>
+          <div 
+          class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+              <div class="col-md-5 p-lg-5 mx-auto my-5" >
+                <h1 class="display-4 font-weight-normal">SMOKEN SEXY DUCK </h1>
+                <h5 class="display-4 font-italic">Build your own world of Games</h5>
+                <p class="lead my-3">  when you are a developer you will be able to upload ur own games and gain money from them , this will cost you only 30$ for LIFETIME </p>
+                <a class="btn btn-outline-secondary" href="{{ url('/bedeveloper') }}">start to be a developer</a>
+              </div>
+              <div class="product-device box-shadow d-none d-md-block"></div>
+              <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
+          
+            </div>  
+      
+
+      
      
+      {{-- @if (count ($games) > 0)
+          
+      @foreach ($games as $game) --}}
+
+
+    {{-- @endforeach
+            <button type ="submit" class="btn btn-primary">Finish Request</button>
+        </form>
+        {{$games->links()}}
+
+    @else
+        <p> No games Found</p>
+
+
+    @endif --}}
     
 
-  <div class="row mb-2">
-    <div class="col-md-6">
-      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">startegy</strong>
-          <h3 class="mb-0">Game 1</h3>
-          <div class="mb-1 text-muted">jun 12</div>
-          <p class="card-text mb-auto">descrition.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">action</strong>
-          <h3 class="mb-0">Game 2</h3>
-          <div class="mb-1 text-muted">jun 12</div>
-          <p class="card-text mb-auto">descrition.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">action</strong>
-          <h3 class="mb-0">Game 3</h3>
-          <div class="mb-1 text-muted">jun 12</div>
-          <p class="card-text mb-auto">descrition.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">puzzle</strong>
-          <h3 class="mb-0">Game 4</h3>
-          <div class="mb-1 text-muted">jun 12</div>
-          <p class="card-text mb-auto">descrition.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">action</strong>
-          <h3 class="mb-0">Game 5</h3>
-          <div class="mb-1 text-muted">jun 12</div>
-          <p class="card-text mb-auto">descrition.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">sports</strong>
-          <h3 class="mb-0">Game 6</h3>
-          <div class="mb-1 text-muted">jun 12</div>
-          <p class="card-text mb-auto">descrition.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">sports</strong>
-          <h3 class="mb-0">Game 7</h3>
-          <div class="mb-1 text-muted">jun 12</div>
-          <p class="card-text mb-auto">descrition.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">action</strong>
-          <h3 class="mb-0">Game 8</h3>
-          <div class="mb-1 text-muted">jun 12</div>
-          <p class="card-text mb-auto">descrition.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-        </div>
-      </div>
-    </div>  
 
 
 
 
 
-
-  </div>
-
-
-
-
-</div>
 
 @endsection
